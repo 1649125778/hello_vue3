b7e15faf486bc4dfbaee3a39f6d82963034d7448
164925778 <1649125778@qq.com>
2025-06-12 17:45:15 +0800

    watch监视 ref监视对象

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,30 +1,43 @@
 <template>
     <div class="person">
-        <h1>情况一：监视【ref】定义的【基本类型】数据</h1>
-        <h2>当前求和为：{{ sum }}</h2>
-        <button @click="addSum">点击求和加一</button>
+        <h1>情况二：监视【ref】定义的【对象类型】数据</h1>
+        <h2>姓名：{{ person.name }}</h2>
+        <h2>年龄：{{ person.age }}</h2>
+        <button @click="changeName">修改名字</button>
+        <button @click="changeAge">修改年龄</button>
+        <button @click="changePerson">修改全部</button>
     </div>
 </template>
 
 <script setup lang="ts" name="Person">
-    import { ref, computed, reactive ,toRefs,watch} from 'vue';
+    import { ref, computed, reactive ,toRefs,watch, watchEffect} from 'vue';
+
     //数据
-    let sum = ref(0)
-
-    //方法
-    function addSum(){
-        sum.value += 1
-    }
-    //监视 情况一：监视【ref】定义的【基本类型】数据
-    const stopWatch = watch(sum,(newValue,oldValue)=>{
-        console.log('newValue：',newValue,'oldValue',oldValue)
-        //停止监视
-        if(newValue>=10){
-            stopWatch
-        }
-
+    let person = ref({
+        name:'zhangsan',
+        age: 18
     })
+    //方法
+    function changeName(){
+        person.value.name += '~'
+    }
+    function changeAge(){
+        person.value.age += 1
+    }
+    function changePerson(){
+        person.value = {name:'李四',age:45}
+    }
+    // 监视 监视的是对象的地址值，若想监视对象内部属性的变化，需要手动开启
+    // watch 第一个参数：监视对象 第二个参数:回调函数 第三个参数：配置对象
+    watch(person,(newValue,oldValue)=>{
+        console.log('newValue:',newValue,'oldValue:',oldValue)
 
+    },{deep:true,immediate:true})
+    
+    // deep 深度监听 监听的属性为对象
+    // immediate 加载时执行
+    // watch的补充
+    // watchEffect  不需要明确监视，谁在回调函数里面使用了,就监视谁 （加载时执行）
 </script>
 
 <style>
