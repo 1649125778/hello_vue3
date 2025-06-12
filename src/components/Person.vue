de57d971d8d8d1605a44ff4d4fd927f562324ce3
164925778 <1649125778@qq.com>
2025-06-12 23:03:06 +0800

    watch 总结

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,3 +1,20 @@
+<!-- 监视总结
+    可是使用ref或者reaction来定义响应式对象，watch函数来监测定义的对象变化
+    ref：定义基本类型的数据 使用watch()可以直接监视
+        定义对象类型的数据 监视的对象的地址值的变化（旧对象 => 新对象）如果想监测对象类型数据中的基本类型数据 需要手动开启深度监视
+    reactive: 定义对象类型的数据 默认开始深度监视
+            如果需要监视对象中的基本类型，则需要写成函数式（（）=> person.name）  因为watch第一个参数可以是【ref对象】，【一个函数，返回一个值】，【一个响应式对象（reactive）】，【以上组成的数组】
+            如果是多个组成的函数 [()=>基本类型，对象类型] 需要开启深度监视， vue中对象监视仅采用浅层监视（只监视对象的地址值变化，不监视对象中的内部属性变化），
+            
+            特性	        对象/数组（引用类型）	  基本类型（值类型）
+            默认监视行为	 仅检测引用变化	           检测值变化
+            { deep: true }	递归监视内部属性变化	  ❌ 无效，被忽略
+            典型使用场景	 嵌套对象、数组元素变化	   独立值变化（如计数器、开关状态）
+
+            [()=>基本类型，对象类型]   监听的是对象类型的地址值 如果点击修改 则替换为新的地址值，无法监听
+
+
+-->
 <template>
     <div class="person">
         <h1>姓名：{{ person.name }}</h1>
@@ -66,19 +83,19 @@
     // 情况四：监视响应式对象中的某个属性，且该属性为基本类型，可以写成函数式,更推荐写函数
     //监视的要是对象中的基本类型属性，最好写函数式
     //如果是对象监视的是地址值，并且需要关注的是对象内部，需要手动开启深度监视
-    watch(
-        ()=>person.car,
-        (newValue,oldValue)=>{
-            console.log('person改变',newValue,oldValue)
-        },
-        {deep:true}
-    )
+    // watch(
+    //     ()=>person.car,
+    //     (newValue,oldValue)=>{
+    //         console.log('person改变',newValue,oldValue)
+    //     },
+    //     {deep:true}
+    // )
 
 
-    // //监视 情况五:监视上述多个数据
-    // watch([()=>person.name,()=>person.car],(newValue,oldValue)=>{
-    //     console.log('person改变',newValue,oldValue)
-    // },{deep:true})
+    // //监视 情况五:监视上述多个数据   如果使用person.car 而不是()=>person.car 比较的是地址值，所以不触发 但是使用函数式则会创建一个新的地址 ，所有会被监测到
+    watch([()=>person.name,person.car],(newValue,oldValue)=>{
+        console.log('person改变',newValue===oldValue)
+    },{deep:true,immediate:true})
 </script>
 
 <style>
