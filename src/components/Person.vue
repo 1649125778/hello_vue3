3895ad5ae515367176a7d73b743f4fe150b20b78
164925778 <1649125778@qq.com>
2025-06-12 15:26:36 +0800

    cpmputed 计算属性 可修改

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,38 +1,54 @@
 <template>
     <div class="person">
-        <h2>姓名: {{ name }}</h2>
-        <h2>年龄: {{ age }}</h2>
-        <button @click="changeName">修改姓名</button>
-        <button @click="changeAge">修改年龄</button>
-   </div>
+        姓：<input type="text" v-model="firstName">
+        <br>
+        名：<input type="text" v-model="lastName">
+        <br>    
+        <button @click="changeFullName">修改全名</button>
+        <br>
+        全名：<span>{{ fullName }}</span>
+    </div>
 </template>
 
 <script setup lang="ts" name="Person">
     import { ref, computed, reactive ,toRefs} from 'vue';
+    let firstName = ref('zhang')
+    let lastName = ref('san')
 
-    let person = reactive({
-        name: '张三',
-        age: 30
+    // 计算属性   有缓存(使用多次但是只调用一次) 只读
+    // let fullName = computed(()=>{
+
+    //     return firtName.value.slice(0,1).toUpperCase+firtName.value.slice(1)+'---'+lastName.value
+    // })
+    //定义可读可写的computed 计算属性
+    let fullName = computed({
+        
+      get(){
+        console.log(firstName.value.slice(0,1).toUpperCase)
+        return firstName.value.slice(0,1).toUpperCase()+firstName.value.slice(1)+'-'+lastName.value
+      },
+      set(val){
+        console.log('set',val)
+        const [str1,str2] = val.split('-')
+        console.log(str1,str2)
+        firstName.value = str1
+        lastName.value = str2
+    }
+        
+
     })
-
-    // toRefs将对象中的全部属性取出，并重新生成一个新的响应式对象
-    // toRef(person,'age') 对象的单个属性
-    let {name,age} = toRefs(person);
-
-
-    function changeName() {
-        name.value += '~~~';
+    function changeFullName(){
+        fullName.value = '123-123'
     }
-
-    function changeAge() {
-        age.value += 1;
-    }
-
 
 </script>
 
 <style>
-
+    .person{
+        border: 1px solid #ccc;
+        padding: 10px;
+        margin: 10px;
+    }
 
 
 </style>
