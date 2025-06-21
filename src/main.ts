7f64fe8d75b0c32cd53c204540c69e6d86a600e5
164925778 <1649125778@qq.com>
2025-06-21 18:49:15 +0800

    搭建pinia环境

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import {createApp} from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//引入pinia
+import { createPinia} from 'pinia'
+//创建pinia
+const pinia = createPinia()
+
+createApp(App).use(pinia).mount('#app')
